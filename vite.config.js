caac69a9ab2282955d7cf5beb20366f260dcedf1
AmmarIbrahim <ammarmalikccg@gmail.com>
2025-02-02 17:36:59 +0500

    Fix file paths in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                publicPage: resolve(__dirname, 'src/pages/public/public.html'),
-                adminPage: resolve(__dirname, 'src/pages/admin/admin.html'),
-            }
-        }
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        publicPage: resolve(__dirname, 'src/pages/public/public.html'),
+        adminPage: resolve(__dirname, 'src/pages/admin/admin.html'),
+      }
     }
+  }
 });
